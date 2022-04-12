a6eb4bd4c0d4abf3e5cc1e5ab54e14aad49b7c3a
movish01 <vermamovish21@gmail.com>
2022-04-12 17:24:00 +0530

    added configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  content: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
